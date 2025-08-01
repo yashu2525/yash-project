dbc994e4f2e5ba5b0c64cb8b74a468e62c1aa672
patel yash <pyash8116@gmail.com>
2025-08-01 19:48:13 +0530

    Dashboard.jsx

---
+++ Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2> Welcome to the Dashboard Page </h2>
+    </div>
+  );
+};
+
+export default Dashboard;
