5bc10fa3370d7337ef395ef79e00b1b50d3ac974
patel yash <pyash8116@gmail.com>
2025-08-05 11:25:57 +0530

    Dashboard.jsx

--- Dashboard.jsx
+++ Dashboard.jsx
@@ -4,6 +4,13 @@ const Dashboard = () => {
   return (
     <div style={{ padding: '20px' }}>
       <h2> Welcome to the Dashboard Page </h2>
+      <div style={{ marginTop: '30px' }}>
+        <ul >
+          <li > Employees</li>
+          <li > Leaves</li>
+          <li > Logout</li>
+        </ul>
+      </div>
     </div>
   );
 };
